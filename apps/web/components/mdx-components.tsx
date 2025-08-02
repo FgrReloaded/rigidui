4178d2aefe14de6348d789e541b5a3b9c3ff17f5
fgrreloaded <nk.nitish.nk500@gmail.com>
2025-08-02 14:02:02 +0530

    remove useless files

--- apps/web/components/mdx-components.tsx
+++ apps/web/components/mdx-components.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
