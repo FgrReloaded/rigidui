d8a0c4762257495dfc2a18e44b57178f59c714d7
fgrreloaded <nk.nitish.nk500@gmail.com>
2025-08-04 08:55:20 +0530

    minor bugs fixed

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -2,11 +2,19 @@ import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
+import { Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
+      <div style={openSans.style}>
+        {children}
+      </div>
     </DocsLayout>
   );
 }
